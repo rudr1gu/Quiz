9035e7fc58b1d5ff53fc41c4a07384b122d9a0da
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-12-31 15:39:11 -0300

    Refactor: Add easy, medium, and hard question files for quiz functionality

---
+++ database/easyQuestion.js
@@ -0,0 +1,91 @@
+export const easy = [{
+    questoes: "Qual atalho é utilizado para copiar e colar?",
+    alternativas: [
+        { text: "CTRL C + CTRL C", correct: false },
+        { text: "CTRL C + CTRL X", correct: false },
+        { text: "CTRL V + CTRL C", correct: false },
+        { text: "CTRL C + CTRL V", correct: true },
+    ]
+},
+{
+    questoes: "Para que serve o roteador Wi-Fi?",
+    alternativas: [
+        { text: "Para gerar internet cabeada.", correct: false },
+        { text: "Para gerar internet móvel", correct: false },
+        { text: "Para gerar conexão sem cabos", correct: true },
+        { text: "Para operadora roubar seus dados pessoais", correct: false },
+    ]
+},
+{
+    questoes: "Qual botão do teclado atualiza o navegador? ",
+    alternativas: [
+        { text: "ALT", correct: false },
+        { text: "F11", correct: false },
+        { text: "F5", correct: true },
+        { text: "ENTER", correct: false },
+    ]
+},
+{
+    questoes: "Para que serve o Pen Drive?",
+    alternativas: [
+        { text: "Armazenamento Online", correct: false },
+        { text: "Armazenamento Rápido", correct: false },
+        { text: "Armazenamento Bluetooth", correct: false },
+        { text: "Armazenamento Portátil", correct: true },
+    ]
+},
+{
+    questoes: "Qual o site mais utilizado para pesquisas?",
+    alternativas: [
+        { text: "Safari", correct: false },
+        { text: "Opera", correct: false },
+        { text: "Google", correct: true },
+        { text: "Firefox", correct: false },
+    ]
+},
+{
+    questoes: "Qual o termo utilizado para quem invade computadores?",
+    alternativas: [
+        { text: "Técnico de celular", correct: false },
+        { text: "Hacker", correct: true },
+        { text: "Desenvolvedor web", correct: false },
+        { text: "Ladrão de casa", correct: false },
+    ]
+},
+{
+    questoes: "Para qual palavra utilizamos a abreviação 'ADD'?",
+    alternativas: [
+        { text: "Adicionar", correct: true },
+        { text: "Adidas ", correct: false },
+        { text: "Android", correct: false },
+        { text: "A doença do computador ", correct: false },
+    ]
+},
+{
+    questoes: "Qual o sistema operacional mais utilizado nos smartphones?",
+    alternativas: [
+        { text: "Windows", correct: false },
+        { text: "Peralix", correct: false },
+        { text: "Linux", correct: false },
+        { text: "Android", correct: true },
+    ]
+},
+{
+    questoes: "Qual é um celular da Apple?",
+    alternativas: [
+        { text: "Galaxy A04", correct: false },
+        { text: "Moto G30", correct: false },
+        { text: "iPhone 11", correct: true },
+        { text: "Redmi 12C", correct: false },
+    ]
+},
+{
+    questoes: "Quais os componentes externos de um computador?",
+    alternativas: [
+        { text: "Processador, teclado, monitor e fonte", correct: false },
+        { text: "Caixa de som, teclado, mouse e impressora", correct: true },
+        { text: "Impressora, placa-mãe e placa de vídeo", correct: false },
+        { text: "Caixa de som, teclado, mouse, impressora e placa-mãe", correct: false },
+    ]
+}
+]
